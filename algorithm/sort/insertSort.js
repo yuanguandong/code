4fbde9992c3bdb85f02f2c1200be2d176206e931
favori <995749721@qq.com>
2021-04-22 10:49:17 +0800

    feat: sort

--- algorithm/sort/insertSort.js
+++ algorithm/sort/insertSort.js
@@ -1,26 +1,34 @@
+// Array.prototype.insertSort = function () {
+//   let arr = this;
+//   for (let i = 1; i < arr.length; i++) {
+//     let j = i;
+//     const temp = arr[j];
+
+//     while (j > 0) {
+//       if (arr[j - 1] > temp) {
+//         arr[j] = arr[j - 1];
+//       } else {
+//         break;
+//       }
+//       j--;
+//     }
+//     arr[j] = temp
+
+//   }
+// };
+
 Array.prototype.insertSort = function () {
-  let arr = this;
-  for (let i = 1; i < arr.length; i++) {
-    let j = i;
-    const temp = arr[j];
-
-    while (j > 0) {
-      if (arr[j - 1] > temp) {
-        arr[j] = arr[j - 1];
-      } else {
-        break;
-      }
-      j--;
+  let temp = this[1];
+  let j = 1;
+  while (j > 0) {
+    if (this[j - 1] > temp) {
+      this[j] = this[j - 1];
     }
-    arr[j] = temp
-
+    j--;
   }
+  this[j] = temp
 };
 
 const arr = [5, 4, 3, 2, 1];
 arr.insertSort();
 console.log(arr);
-
-
-
-
--- algorithm/sort/selectionSort.js
+++ algorithm/sort/selectionSort.js
@@ -1,16 +1,32 @@
+// Array.prototype.selectionSort = function () {
+
+//   let arr = this;
+//   for (let i = 0; i < arr.length-1; i++) {
+//     let indexMin = i;
+//     for (let j = i; j < arr.length; j += 1) {
+//       if (arr[j] < arr[indexMin]) {
+//         indexMin = j;
+//       }
+//     }
+//     let temp = arr[i];
+//     arr[i] = arr[indexMin];
+//     arr[indexMin] = temp;
+//   }
+// };
+
 Array.prototype.selectionSort = function () {
-  
   let arr = this;
-  for (let i = 0; i < arr.length-1; i++) {
-    let indexMin = i;
-    for (let j = i; j < arr.length; j += 1) {
-      if (arr[j] < arr[indexMin]) {
-        indexMin = j;
+  
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i;
+    for (let j = i; j < arr.length; j++) {
+      if(arr[j] < arr[minIndex]){
+        minIndex = j
       }
     }
-    let temp = arr[i];
-    arr[i] = arr[indexMin];
-    arr[indexMin] = temp;
+    let temp = arr[i]
+    arr[i] = arr[minIndex]
+    arr[minIndex] = temp
   }
 };
 
