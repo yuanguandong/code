9869f27386ec132d97c767e902fa6ae536db7b5e
favori <995749721@qq.com>
2021-05-30 16:20:13 +0800

    fix: 动态规划

--- algorithm/rec/sum/dc.js
+++ algorithm/rec/sum/dc.js
@@ -1,26 +1,41 @@
-const sum = (arg)=>{
-   if(arg.length <=1){
-     return arg[0]
-   }else{
-     const a = arg.shift()
-     return a + sum(arg)
-   }
-}
+//递归求和
+// const sum = (arg)=>{
+//    if(arg.length <=1){
+//      return arg[0]
+//    }else{
+//      const a = arg.shift()
+//      return a + sum(arg)
+//    }
+// }
 
-const b= sum([1,2,3])
+// const b= sum([1,2,3])
+
+//递归求长度
+// const length = (arr,total)=>{
+//   if(!total){total=0}
+//   const node = arr.shift()
+//   if(node){
+//     return length(arr,total+1)
+//   }else{
+//     return total
+//   } 
+// }
+
+// const c = length([1,2,3,4,5])
 
 
-const length = (arr,total)=>{
-  if(!total){total=0}
+// debugger
+//递归找最大值
+const findMax = (arr,max)=>{
+  if(!max){max=arr[0]}
+  if(arr.length===0){return max}
   const node = arr.shift()
-  if(node){
-    return length(arr,total+1)
+  if(node>max){
+    return findMax(arr,node)
   }else{
-    return total
-  } 
+    return findMax(arr,max)
+  }
 }
 
-const c = length([1,2,3,4,5])
-
-
+const max = findMax([1,2,66,22,55,888])
 debugger
