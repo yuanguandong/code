7fb9cd96e43f5b9466ae6bce1bd31f0b551acc05
yuanguandong <995749721@qq.com>
2021-06-18 08:07:59 +0800

    chore: 全排列

--- algorithm/whole.js
+++ algorithm/whole.js
@@ -17,29 +17,52 @@ let arr = ["a", "b", "c", "d"];
 
 // p(arr);
 
-function getGroup(arr, index, group) {
-  let arrTemp = [];
-  arrTemp.push(arr[index]);
-  for (let i = 0; i < group.length; i++) {
-    arrTemp.push(group[i] + arr[index]);
-  }
-  group = group.concat(arrTemp);
-  if (index >= arr.length-1) {
-    return group;
-  } else {
-    return getGroup(arr, index + 1, group);
-  }
-}
+// function getGroup(arr, index, group) {
+//   let arrTemp = [];
+//   arrTemp.push(arr[index]);
+//   for (let i = 0; i < group.length; i++) {
+//     arrTemp.push(group[i] + arr[index]);
+//   }
+//   group = group.concat(arrTemp);
+//   if (index >= arr.length-1) {
+//     return group;
+//   } else {
+//     return getGroup(arr, index + 1, group);
+//   }
+// }
 
-let a = getGroup(arr, 0, []);
+// let a = getGroup(arr, 0, []);
 
-const whole=(n)=>{
-  let arr1 = []
-  for(let i=1; i<n;i++){
-    arr1.push(String(i))
+// const whole=(n)=>{
+//   let arr1 = []
+//   for(let i=1; i<n;i++){
+//     arr1.push(String(i))
+//   }
+//   return getGroup(arr1, 0, [])
+// }
+
+// let b = whole(5)
+// console.log('b',b)
+
+
+
+var permute = function(nums) {
+  const res = []
+  const recursion = (arr = []) => {
+      if (arr.length === nums.length) {
+          res.push(arr)
+          return
+      }
+      for (let i = 0; i < nums.length; i++) {
+          if (!arr.includes(nums[i])) {
+              recursion([...arr, nums[i]])
+          }
+      }
   }
-  return getGroup(arr1, 0, [])
-}
+  recursion([])
+  return res
+};
 
-let b = whole(5)
-console.log('b',b)
+
+const b = permute(arr)
+debugger
