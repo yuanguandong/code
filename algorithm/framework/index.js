1a0c144a5bff87b569ce7bef327976640682eef8
yuanguandong <995749721@qq.com>
2021-07-02 23:12:18 +0800

    fix: 算法框架

---
+++ algorithm/framework/index.js
@@ -0,0 +1,14 @@
+// 滑动窗口
+let left = 0, right = 0;
+
+while (right < s.size()) {
+    // 增大窗口
+    window.add(s[right]);
+    right++;
+
+    while (window needs shrink) {
+        // 缩小窗口
+        window.remove(s[left]);
+        left++;
+    }
+}
