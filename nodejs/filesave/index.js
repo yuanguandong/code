dcc1a70d5d6a6a725b6c38c9c4d179d6a54f5515
yuanguandong <995749721@qq.com>
2021-08-28 13:13:01 +0800

    chore: update

--- nodejs/filesave/index.js
+++ nodejs/filesave/index.js
@@ -1,25 +1,27 @@
 var ghdownload = require("github-download"),
   exec = require("exec");
 
+console.log(process.cwd());
+const defaultUrl = process.cwd() + "/widgets";
 ghdownload(
-  { user: "yuanguandong", repo: "react-dashboard-pro", ref: "master" },
-  process.cwd()
-)
-  .on("dir", function (dir) {
-    console.log(dir);
-  })
-  .on("file", function (file) {
-    console.log(file);
-  })
-  .on("zip", function (zipUrl) {
-    //only emitted if Github API limit is reached and the zip file is downloaded
-    console.log(zipUrl);
-  })
-  .on("error", function (err) {
-    console.error(err);
-  })
-  .on("end", function () {
-    exec("tree", function (err, stdout, sderr) {
-      console.log(stdout);
-    });
-  });
+  { user: "yuanguandong", repo: "react-dashboard-pro", ref: "master",dir:"/widgets" },
+  defaultUrl
+);
+// .on("dir", function (dir) {
+//   console.log(dir);
+// })
+// .on("file", function (file) {
+//   console.log(file);
+// })
+// .on("zip", function (zipUrl) {
+//   //only emitted if Github API limit is reached and the zip file is downloaded
+//   console.log(zipUrl);
+// })
+// .on("error", function (err) {
+//   console.error(err);
+// })
+// .on("end", function () {
+//   exec("tree", function (err, stdout, sderr) {
+//     console.log(stdout);
+//   });
+// });
