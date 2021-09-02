dfd094dca7c124fc52ff51c2468be83c36cf7087
yuanguandong <995749721@qq.com>
2021-09-02 22:45:19 +0800

    chore: drawer

--- reactdemo/src/demo/batchedupdate/index.jsx
+++ reactdemo/src/demo/batchedupdate/index.jsx
@@ -1,82 +1,82 @@
 import React, { useState, useLayoutEffect } from "react";
 
-// export default class Index extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       number: 0,
-//     };
-//   }
-//   handerClick = () => {
-//     // for (let i = 0; i < 5; i++) {
-//      setTimeout(()=>{
-//          this.setState({ number:this.state.number+1 })
-//          console.log(this.state.number)
-//          this.setState({ number:this.state.number+1 })
-//          console.log(this.state.number)
-//          this.setState({ number:this.state.number+1 })
-//          console.log(this.state.number)
-//      },1000)
-//     // Promise.resolve().then(() => {
-//     //   // ReactDOM.unstable_batchedUpdates(() => {
-//     //     this.setState({ number: this.state.number + 1 });
-//     //     console.log(this.state.number);
-//     //     this.setState({ number: this.state.number + 1 });
-//     //     console.log(this.state.number);
-//     //     this.setState({ number: this.state.number + 1 });
-//     //     console.log(this.state.number);
-//     //   // });
-//     // });
-//     // }
-//   };
-
-//   render() {
-//     return (
-//       <div>
-//         <button onClick={this.handerClick}>num++</button>
-//       </div>
-//     );
-//   }
-// }
-
-export default () => {
-  const [num, setNum] = useState(0);
-  const handerClick = () => {
-    setNum(num + 1);
-    console.log(num);
-    setNum(num + 1);
-    console.log(num);
-    setNum(num + 1);
-    console.log(num);
-    // setTimeout(() => {
-    //   // setNum(num + 1);
-    //   // console.log(num)
-    //   // setNum(num + 1);
-    //   // console.log(num)
-    //   // setNum(num + 1);
-    //   // console.log(num)
-    // }, 1000);
-    // // setNum(num + 1);
-    // // setNum(num + 1);
-    // // setNum(num + 1);
-    // Promise.resolve().then(() => {
-    //   setNum(num + 1);
-    //   console.log(num)
-    //   setNum(num + 1);
-    //   console.log(num)
-    //   setNum(num + 1);
-    //   console.log(num)
-    // })
+export default class Index extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: 0,
+    };
+  }
+  handerClick = () => {
+    setTimeout(() => {
+      this.setState({ number: this.state.number + 1 });
+      console.log(this.state.number);
+      this.setState({ number: this.state.number + 1 });
+      console.log(this.state.number);
+      this.setState({ number: this.state.number + 1 });
+      console.log(this.state.number);
+    }, 1000);
+    Promise.resolve().then(() => {
+      // ReactDOM.unstable_batchedUpdates(() => {
+      this.setState({ number: this.state.number + 1 });
+      console.log(this.state.number);
+      this.setState({ number: this.state.number + 1 });
+      console.log(this.state.number);
+      this.setState({ number: this.state.number + 1 });
+      console.log(this.state.number);
+      // });
+    });
   };
 
-  useLayoutEffect(() => {
-    const dom = document.getElementById("button");
-    if (!dom) return;
-    dom.addEventListener("click", handerClick);
-  }, []);
-  return (
-    <div>
-      <button id="button">{num}</button>
-    </div>
-  );
-};
+  render() {
+    return (
+      <div>
+        <button onClick={this.handerClick}>num++</button>
+      </div>
+    );
+  }
+}
+
+// export default () => {
+//   const [num, setNum] = useState(0);
+//   const handerClick = () => {
+
+//     setNum(num + 1);
+//     console.log(num);
+//     setNum(num + 1);
+//     console.log(num);
+//     setNum(num + 1);
+//     console.log(num);
+
+//     setTimeout(() => {
+//       setNum(num + 1);
+//       console.log(num);
+//       setNum(num + 1);
+//       console.log(num);
+//       setNum(num + 1);
+//       console.log(num);
+//     }, 1000);
+
+//     Promise.resolve().then(() => {
+//       setNum(num + 1);
+//       console.log(num);
+//       setNum(num + 1);
+//       console.log(num);
+//       setNum(num + 1);
+//       console.log(num);
+//     });
+//   };
+
+//   useLayoutEffect(() => {
+//     const dom = document.getElementById("button");
+//     if (!dom) return;
+//     dom.addEventListener("click", handerClick);
+//   }, []);
+
+//   console.log('render',num)
+//   return (
+//     <div>
+//       <button id="button">{num}</button>
+//     </div>
+//   );
+// };
