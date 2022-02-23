9b6851e3963830e834152ff4025928494049240d
Favori <995749721@qq.com>
2022-02-23 08:13:40 +0800

    chore: update

--- nodejs/cli-test/ring/index.tsx
+++ nodejs/cli-test/ring/index.tsx
@@ -1,23 +1,24 @@
-import { ClockCircleOutlined } from '@ant-design/icons';
+import { IssuesCloseOutlined } from '@ant-design/icons';
 import Panel from './component';
 import snapShot from './snapshot.png';
 
 export default {
-  name: 'Clock',
-  description: 'a clock',
-  tags: ['all','system'],
+  name: 'Ring',
+  description: 'ring progress',
+  tags: ['all','chart'],
   component: Panel,
   configComponent: null,
   maxLength: 2,
   snapShot,
-  icon: <ClockCircleOutlined />,
-  iconBackground: 'blue',
+  icon: <IssuesCloseOutlined />,
+  iconBackground: 'orange',
   size: {
-    defaultWidth: 4,
+    defaultWidth: 2,
     defaultHeight: 5,
     maxWidth: 12,
     maxHeight: 16,
     minWidth: 2,
     minHeight: 4,
   },
+  author:'Favori'
 }
