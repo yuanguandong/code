6cafc30e0e8269bcbdf09bc2e7ac93144b0bba4a
yuanguandong <yuangd@gantsoftware.com>
2022-11-13 10:47:04 +0800

    chore: update

--- docker/demoweb/server.js
+++ docker/demoweb/server.js
@@ -1,10 +1,12 @@
-const Koa = require('koa');
+const Koa = require("koa");
 const app = new Koa();
+const fs = require("fs"); //引入他的模块之后调用读取文件的方法
+const data = fs.readFileSync("file.txt", "utf-8");
 
 // response
-app.use(ctx => {
-    ctx.body = 'Hello Koa 111';
+app.use((ctx) => {
+  ctx.body = data;
 });
 
 app.listen(3000);
-console.log('Server running at http://127.0.0.1:3000')
+console.log("Server running at http://127.0.0.1:3000");
